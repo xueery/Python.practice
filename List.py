58ce8e8c184bbae160c4ebc9d8485117e4a7b53d
xueery <2042064451@qq.com>
2019-11-28 12:42:56 +0800

    序列类型

---
+++ List.py
@@ -0,0 +1,24 @@
+#元组：
+S=tuple("123")
+print(S)
+print(len(S))
+#列表
+ls=[1,2,3,4,5]
+print(ls)
+ls+=[1,2]
+print(ls)
+ls[0]=10
+print(ls)
+del ls[3]
+print(ls)
+ls*=3
+print(ls)
+ls.clear()
+print(ls)
+ls.append(2)
+print(ls)
+ls+=[3,4,5]
+print(ls)
+ls.reverse()
+print(ls)
+print(3 in ls)
